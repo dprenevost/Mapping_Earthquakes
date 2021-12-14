f8f870fb2fccd7e0f85ab478bab4d6da2a608a6e
dprenevost <dprenevost@yahoo.com>
2021-12-13 18:54:57 -0600

    13.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,10 +2,6 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-// // basic way
-// let map = L.map('mapid').setView([40.7, -94.5], 4);
-// // better way for layering
-// Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
         40.7, -94.5
@@ -13,28 +9,7 @@ let map = L.map("mapid", {
     zoom: 4
 });
 
-
 // // Title layer from leaflet.com
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(map);
-// We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
-
 // We create the tile layer that will be the background of our map.
 // let streets =// We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -42,50 +17,9 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-// //  Add a marker to the map for Los Angeles, California.
-// let cities = L.cities([34.0522, -118.2437]).addTo(map);
-// L.circle([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "black",
-//     fillColor: '#ffffa1'
-
-//  }).addTo(map);
- // An array containing each city's location, state, and population.
-// let cities = [{
-//     location: [40.7128, -74.0059],
-//     city: "New York City",
-//     state: "NY",
-//     population: 8398748
-//   },
-//   {
-//     location: [41.8781, -87.6298],
-//     city: "Chicago",
-//     state: "IL",
-//     population: 2705994
-//   },
-//   {
-//     location: [29.7604, -95.3698],
-//     city: "Houston",
-//     state: "TX",
-//     population: 2325502
-//   },
-//   {
-//     location: [34.0522, -118.2437],
-//     city: "Los Angeles",
-//     state: "CA",
-//     population: 3990456
-//   },
-//   {
-//     location: [33.4484, -112.0740],
-//     city: "Phoenix",
-//     state: "AZ",
-//     population: 1660272
-//   }
-//   ];
-  // Loop through the cities array and create one marker for each city.
+// Loop through the cities array and create one marker for each city.
 // Get data from cities.js
 let cityData = cities;
 // Loop through the cities array and create one marker for each city.
@@ -93,7 +27,8 @@ cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
         radius: city.population/100000,
-        color: "orange"
+        color: "orange",
+        weight: 4
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
